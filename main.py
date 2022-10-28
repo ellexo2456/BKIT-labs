bc3eb65c39c30ffac90b28010109c758ddc6f3a9
Alexey <ax.chinaev@yandex.ru>
2022-10-28 11:32:28 +0300

    done but not beautiful

--- main.py
+++ main.py
@@ -1,64 +1,117 @@
-import sys
-import math
+from operator import itemgetter
 
 
-def get_coef(index, prompt):
-    try:
-        coef_str = sys.argv[index]
-    except:
-        print(prompt)
-        coef_str = input()
-    try:
-        coef = float(coef_str)
-        if index == 1 and coef == 0:
-            raise ValueError('Not biquadratic equation')
-    except:
-        print('Неккоректная форма аргумента. Пожалуйста, введите аргумент в виде действительного числа')
-        return get_coef('not console input', prompt)
-    return coef
+class Faculty:
+    def __init__(self, name, id, university_id, students_count):  # Constructor
+        self.name = name
+        self.id = id
+        self.university_id = university_id
+        self.students_count = students_count
 
 
-def get_roots(a, b, c):
-    roots = []
-    D = b * b - 4 * a * c
-    if D == 0.0:
-        root = -b / (2.0 * a)
-        if root >= 0:
-            roots.append(root)
-    elif D > 0.0:
-        sqD = math.sqrt(D)
-        root1 = (-b + sqD) / (2.0 * a)
-        root2 = (-b - sqD) / (2.0 * a)
-        roots.append(root1)
-        roots.append(root2)
+class University:
+    def __init__(self, id, name):  # Constructor
+        self.id = id
+        self.name = name
+
+
+# Многие-ко-многим
+class FacultyUniversity:
+    def __init__(self, faculty_id, university_id):  # Constructor
+        self.faculty_id = faculty_id
+        self.university_id = university_id
+
+
+# Students
+university = [
+    University(1, "BMSTU"),
+    University(2, "SPSU"),
+    University(3, "Moscow State University"),
+    University(4, "Novosibirsk State University"),
+    University(5, "TSU"),
+]
+
+# Groups
+faculty = [
+    Faculty("FN4", 1, 1, 1000),
+    Faculty("Georgafical", 2, 2, 2343),
+    Faculty("mathematical", 3, 4, 3524),
+]
+
+# Students and Groups
+faculty_university = [
+    FacultyUniversity(1, 1),
+    FacultyUniversity(1, 2),
+    FacultyUniversity(2, 3),
+    FacultyUniversity(3, 4),
+    FacultyUniversity(3, 5),
+]
+
+
+# University и Faculty связаны отношением один-ко-многим.
+# Выведите список всех университетов, у которых в названии присутствует
+# слово "University", и факультетов в них.
+
+def task1():
     result = []
-    for i in roots:
-        if i > 0:
-            result.append(math.sqrt(i))
-            result.append(-1 * math.sqrt(i))
-        if i == 0:
-            result.append(i)
+    for i in university:
+        middle_result = []
+        if "University" in i.name:
+            middle_result.append(i.name)
+            for j in faculty:
+                if j.university_id == i.id:
+                    middle_result.append(j.name)
+            result.append(middle_result)
+    return result
+
+
+# University и Faculty связаны отношением один-ко-многим.
+# Выведите список всех университетов со средним количеством студентов в каждом факультете,
+# отстортированный по среднему количеству. Среднее количество должно быть округлено до 2 знаков после запятой
+
+def task2():
+    result = []
+    for i in university:
+        sum = 0
+        count = 0
+        for j in faculty:
+            if j.university_id == i.id:
+                sum += j.students_count
+                count += 1
+        if (count == 0):
+            result.append((i.name, 0))
+        else:
+            result.append((i.name, round(sum / count, 2)))
+    return sorted(result, key=itemgetter(1), reverse=True)
+
+
+# University и Faculty связаны отношением многие-ко-многим.
+# Выведите список всех факультетов, у которых название начинается с буквы "G",
+# и список вузов, в которых имеется такой факультет.
+
+def task3():
+    result = []
+    for i in faculty:
+        if i.name[0] == "G":
+            result.append(i.name)
+            for j in faculty_university:
+                if i.id == j.university_id:
+                    for k in university:
+                        if j.university_id == k.id:
+                            result.append(k.name)
     return result
 
 
 def main():
-    a = get_coef(1, 'Введите коэффициент А:')
-    b = get_coef(2, 'Введите коэффициент B:')
-    c = get_coef(3, 'Введите коэффициент C:')
-    roots = get_roots(a, b, c)
-    len_roots = len(roots)
-    if len_roots == 0:
-        print('Нет корней')
-    elif len_roots == 1:
-        print('Один корень: {}'.format(roots[0]))
-    elif len_roots == 2:
-        print('Два корня: {} и {}'.format(roots[0], roots[1]))
-    elif len_roots == 3:
-        print('Три корня: {} и {} и {}'.format(roots[0], roots[1], roots[2]))
-    elif len_roots == 4:
-        print('Четыре  корня: {} и {} и {} и {}'.format(roots[0], roots[1], roots[2], roots[3]))
+    print("\nTask 1:")
+    print(task1())
+
+    print("\nTask 2:")
+    print(task2())
+
+    print("\nTask 3:")
+    print(task3())
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
